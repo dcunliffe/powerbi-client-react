171930a08d355a89af07df6b5b563b154df9aaa5
David Cunliffe <david.l.cunliffe@gmail.com>
2024-06-05 16:47:32 +0100

    Update DemoApp.tsx

--- React/demo/src/DemoApp.tsx
+++ React/demo/src/DemoApp.tsx
@@ -15,6 +15,7 @@ function DemoApp (): JSX.Element {
 
 	// PowerBI Report object (to be received via callback)
 	const [report, setReport] = useState<Report>();
+	const [pseudo, setPseudo] = useState<string>();
 
 	// Track Report embedding status
 	const [isEmbedded, setIsEmbedded] = useState<boolean>(false);
@@ -47,6 +48,8 @@ function DemoApp (): JSX.Element {
 		['rendered', () => console.log('Report has rendered')],
 		['error', (event?: service.ICustomEvent<any>) => {
 				if (event) {
+
+
 					console.error(event.detail);
 				}
 			},
@@ -82,12 +85,14 @@ function DemoApp (): JSX.Element {
 		}
 
 		const reportConfig = await reportConfigResponse.json();
-
+		console.log(reportConfig.EmbedUrl);
 		// Update the reportConfig to embed the PowerBI report
 		setReportConfig({
 			...sampleReportConfig,
-			embedUrl: reportConfig.EmbedUrl,
-			accessToken: reportConfig.EmbedToken.Token
+			//embedUrl: reportConfig.EmbedUrl,
+			embedUrl: "https://app.powerbi.com/reportEmbed?reportId=13e17eb9-47a4-436a-9ef8-87738d04741d&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVLLVNPVVRILXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZX19",
+			//accessToken: reportConfig.EmbedToken.Token
+			accessToken: "H4sIAAAAAAAEAB2TtY7FBgAE_-VaRzJTpBRmZnZnfIZnZkf591zST7Ozmr9_7Oz5Tln58-dPJR7lcbjv5zH31eAa3LXck1Ki-CK6wFto5BMoRz5fGT9r1gUKeh37HGr4SpbWdUblJ35z1PnYGllZtt8ShjublpzQk_Eaw5VkBDR2OvrE0E1hK4huA4yBcR2bpgCUcP-q5F592rJwL03CLASxWO0CofTjgq-TMWQKo-Mcv2LF7TpYGFqZDPiTRAaOYkw4fo7YM_eWGwfT0x3n7FeA4TA3XCm9hCTuAeIo9G6ydoWCXjqzWvYMdIgdMAPoVlDD2Zb3KG2r0FF9yzEeNSA1qrw6rRLuHKi2mSdHAj7fR7hIEDbfsxaiwP8c8koxzt4MBBKqRn2peDTKl1Z8u1ocCIEpTre5Zftu9vBVIQwL-SBpWtcbXyems22PXLYhQynXJZ3xbsm0q6c-B9jkB2jPgRF5IXTtIskXvi9lIPVEa8F4yjHdrW1seiNZFKLCH7kqtdxEUHfOePXvgP3iEo-9SITzDzX3koS_UinhIZskcfXZxKKchOM-r3iSvng-DD1EtuxS-tdnkvGhpO1oVNxjOLuK5tGZi7XTQni4Z7XesHLhPDbG0AcKgrcQWPBs-NBJaL3AKcuFWmj5mTgsDLEoV7LrYKwt7pNcpiOPf8itmqrYNwxV3ibF0OrmtSQYys3G9MhlmX9Qq8VvWiev764SoFea-1htrQsjyyMz1qOk6cahxboIeM728Bx5UNllj7LKb7Dq5DrE25YuyKy8WhOaho6zKSKaQj5AIxBGUp7O8rdTGXyK1lsDX6n4FnhjnQAMmcNV1EFpDtvrAGszyJudM2fSL358z8CLIcikTOj6QFylJaDDUn_9_PHDrc-8T1r1_KZTLoYdXvxBY_psYs2l1Hn3RCwqbsS4b37hAunS9yoUnoKX1OoNizFuweS5M_1g67b4idb1SyzgDru7KElpn5hndN39rOogFL2nLVttlonf1rWT0x2GIPktIUGjXGU0U-jQuDDEI5PyLa7nG76TtGvx-Qa1m4Ka57xouGkhG1Ucp3QyMKxIU_DIWSYJPB4fUllIUFXBN69PWEa5l7ixnTdjTlcXmZZ7XFDk_BtdCw2QN9OMH-jTwyuxWwXqm8ZXSJD0DC8OyWYBg6sJ896XpDc74oAnQhK6uebTmiIkpbtRJvxXUG_oDtRdn7PWL_lVUuC6Yq2TzY7XOPEPzrhgo0Y1bTh__a_5mZtqVcJfywgbgKxq4sxsZQEnp4oYyzjzP-W1nzHbj7X6xRLaqpjdAuYJJ3hsNe1JFKZQIcPLvW6l-21k56txa-NZ2V2yXaTKQF5DSm18t18m-pQUz-yr4qXsIDg4BA4aiXQ3Q_JhaniNdQX4zLN3fVDKWMW9hT17dHm0o1QN_wXT10mvlD5nK386NwIu61qGvvF0CwJfkEfL6eCyJeZEFBlfGKHKwwfKEchxPJktOccCCorDLe7OEWhJ8GLRkAEitbBmniult3DbGhO18KU3PDCEq3FKZu2tos9I-Bx0Tu9T3_bNeQNPk7RtOw2iG08Hi5F76_kCkR7YGuvMsN5vkFMt33WPLL8GamzIKN2aB-gRWzyYe1ChRw_wmff1MBuYFar_74x__gUq4qOGLgYAAA==.eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVLLVNPVVRILXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZXhwIjoxNzE3NjA0Mzc3LCJhbGxvd0FjY2Vzc092ZXJQdWJsaWNJbnRlcm5ldCI6dHJ1ZX0="
 		});
 		setIsEmbedded(true);
 
@@ -137,7 +142,16 @@ function DemoApp (): JSX.Element {
 	const setDataSelectedEvent = () => {
 		setEventHandlersMap(new Map<string, (event?: service.ICustomEvent<any>, embeddedEntity?: Embed) => void | null> ([
 			...eventHandlersMap,
-			['dataSelected', (event) => console.log(event)],
+			['dataSelected', (event) => {
+				if(event){
+					console.log(event.detail.dataPoints[0].identity[0].equals)
+					setPseudo(event.detail.dataPoints[0].identity[0].equals)
+				}
+				console.log(event)
+
+
+			}
+			],
 		]));
 
 		setMessage('Data Selected event set successfully. Select data to see event in console.');
@@ -253,7 +267,10 @@ function DemoApp (): JSX.Element {
 
 				{ isEmbedded ? reportComponent : null }
 			</div>
-
+			<div>
+				{pseudo ? "Selected Pseudo: " :""}
+				{pseudo}
+			</div>
 			{ footer }
 		</div>
 	);
